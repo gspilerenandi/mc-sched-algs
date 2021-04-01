c602e2dc778524c5ffa220be31d4bcd58f5978cf
gspilerenandi <giannnandi@gmail.com>
2021-04-01 10:07:05 +0100

    Update huangchen.m
    reordering of the table of tasks

--- huangchen.m
+++ huangchen.m
@@ -1,27 +1,19 @@
-%   C = WCET
-%   T = minimum inter-arrival time
-%   D = relative deadline
-%   (row, column)
-
 %   uunifast single core tasks utilization: 4 tasks; utilization bound 0.55
 %   columns are tasks and rows are modes: 4 tasks and 3 modes
-%   FTP analysis: the priority of the task is the only thing that matters
-%   priority ordering -> ascending right to left in this case (RM = prio inversely proportional to the period)
-%   modos -> linhas, task -> colunas
-%   t1.1                    t2.1                    t3.1                    t4.1
-%   t1.2                    t2.2                    t3.2                    t4.2
-u = [0.41130999300361837    0.0014241633805865128   0.10954825924790529     0.027717584367889847;
-     0.07770052548160622    0.13101584660539967     0.013513632001002363    0.3277699959119918;
-     0.0698871955687706     0.192184073329005       0.14165934558077242     0.14626938552145202];
+u = [0.027717584367889847   0.10954825924790529    0.0014241633805865128        0.41130999300361837;
+     0.3277699959119918     0.013513632001002363   0.13101584660539967          0.07770052548160622;
+     0.14626938552145202    0.14165934558077242    0.192184073329005            0.0698871955687706];
+
 %   number of tasks
 n_tasks = size(u,2);
 %   number of modes
 n_modes = size(u,1);
 
-%   arbitrary periods
-t = [200      125     80    60;
-     250      150     70    50;
-     170      100     75    35];
+%   arbitrary periods (lower prio ------> higher prio)
+t = [100,     85,    70,   60
+     100,     70,    55,   50
+     80,      55,    40,   35];
+ 
 %   implicit deadlines -> deadline = period
 d = t;
         
@@ -31,7 +23,7 @@ c = u.*t;
 %   starts the analysis from the lowest priority task
 task_k = 1;
 %   fixed mode for the FTP, as all modes of a task have the same priority
-mode = 2;
+mode = 3;
 
 %   starts the analysis assuming the system is schedulable
 is_sched = true;
@@ -61,6 +53,7 @@ while ( (task_k < n_tasks) && is_sched )
     end
 
     %   returns true or false for the inequality
+    fprintf('TheoremPt1 i = %i, Ci =  %i \n', task_k, sum_Ci);
     theorem1_pt1 = d(mode, task_k) - sum_Ci - c(mode, task_k) >= 0 ;
 
     %   calculates the second part of theorem 1 (still needs to be correctly indexed according to betha)
@@ -75,7 +68,9 @@ while ( (task_k < n_tasks) && is_sched )
         sum_Ci = sum_Ci + max( c( :, betha_index(i) + task_k ) );
     end
     %   returns true or false for the inequality
+    fprintf('TheoremPt2 i = %i Ui = %i,  Ci = %i \n', task_k, sum_Ui, sum_Ci);
     theorem1_pt2 = c(mode, task_k) <= d(mode, task_k) - sum_Ui - sum_Ci;
+    fprintf('Theorem1 %d %d\n', theorem1_pt1, theorem1_pt2)
     is_sched = theorem1_pt1 && theorem1_pt2;
     %   increments the task counter
     task_k = task_k + 1;
